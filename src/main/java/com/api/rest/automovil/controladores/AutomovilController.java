7d99f0227495b4c81a3e1f574fae11532613b416
Jesus Alberto Diaz Dominguez <albertodiaz.42069@gmail.com>
2022-07-18 15:00:52 -0500

    enlace con pagina web
    agregar linea e importar framework

--- src/main/java/com/api/rest/automovil/controladores/AutomovilController.java
+++ src/main/java/com/api/rest/automovil/controladores/AutomovilController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.api.rest.automovil.entidades.Automovil;
 import com.api.rest.automovil.repositorios.AutomovilRepository;
 
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/automovil")
 public class AutomovilController {
--- src/main/java/com/api/rest/automovil/controladores/ModeloController.java
+++ src/main/java/com/api/rest/automovil/controladores/ModeloController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,8 @@ import com.api.rest.automovil.entidades.Modelo;
 import com.api.rest.automovil.repositorios.AutomovilRepository;
 import com.api.rest.automovil.repositorios.ModeloRepository;
 
+
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/modelos")
 public class ModeloController {
